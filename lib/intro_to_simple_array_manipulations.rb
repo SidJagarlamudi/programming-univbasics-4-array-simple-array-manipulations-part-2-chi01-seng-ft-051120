92b0c6e11732f56ffc5f86e0a5073c7d918f8439
Sid Jagarlamudi <jaglings@gmail.com>
2020-04-13 22:32:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,8 +11,8 @@ def using_insert(list_of_esoteric_programming_languages, another_esoteric_langua
 end
 
 def using_uniq(captain_planet_and_the_planeteers)
-  @captain_planet_and_the_planeteers = ["Captain Planet", "Gaia", "Kwame", "Gi", "Linka", "Wheeler", "Gaia"]
-@new_array = @captain_planet_and_the_planeteers.uniq
+  captain_planet_and_the_planeteers = ["Captain Planet", "Gaia", "Kwame", "Gi", "Linka", "Wheeler", "Gaia"]
+new_array = captain_planet_and_the_planeteers.uniq
 end
 
 def using_flatten(private_colleges_in_newyork)
